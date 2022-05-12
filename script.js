cece7b64539e9d2d5e9e141086d1c870e372d850
Pravaritti <pravaritti.1995@gmail.com>
2022-05-12 20:41:25 +0530

    Test Geolocation API

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//geolocation API is actually a browser API that the browser gives us like timer and other APIs
+//it takes 2 callback functions 1. success callback(when it gets coordinates successfully) 2. error callback
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords; //destructuring
+      const { longitude } = position.coords; //destructuring
+      console.log(latitude, longitude);
+      //checking it on google maps
+      console.log(`https://www.google.co.in/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
